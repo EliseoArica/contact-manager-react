c08b923d77cadb30c7e9f6119bde486e62dd4da0
Eliseo Arica <eliseo989665091@gmail.com>
2022-07-18 17:03:22 -0500

    Progreso 18-07-22 tarde

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import  ReactDOM  from "react-dom/client";
 import "./index.css";
 
@@ -24,11 +24,12 @@ class Item extends React.Component {
 
 class App extends React.Component {
     render() {
-        return <>
+        return <div className="section">
+            <h1>First steps with React</h1>
             <Item name="Cheese" price="4.50" />
             <Item name="Bread" price="1.30" />
             <Item name="Ice cream" price="24.59" />
-        </>
+        </div>
     }
 }
 
@@ -37,14 +38,10 @@ class Hello extends React.Component {
         name: "Ayaka",
         age: 20
     }
-    checkCondition() {
-        if (1 > 0) {
-            this.setState({name: "Yoimiya", age: 19});
-        }
-    }
     render () {
-        this.checkCondition();
-        return <h1>Hello {this.state.name}, {this.state.age}</h1>;
+        return <div className="section">
+            <h2 className="subtitle">Hello {this.state.name}, {this.state.age}</h2>
+        </div>;
     }
 }
 
@@ -57,9 +54,10 @@ class Counter extends React.Component {
         this.setState({counter: this.state.counter + 1});
     }
     render() {
-        return <div>
-            <p>{this.state.counter}</p>
-            <button onClick={this.increment}>Increment</button>
+        return <div className="section">
+            <h2 className="subtitle">Counter using setState in class Components</h2>
+            <p className="p">{this.state.counter}</p>
+            <button className="button--increment" onClick={this.increment}>Increment</button>
         </div>
     }
 }
@@ -69,7 +67,9 @@ class Counter extends React.Component {
 // ------------------------------------------------------------------------
 function HelloFunctional() {
     const [name] = useState("Ayaka"); // Not setName cause will not use it.
-    return <h1>Hello {name}.</h1>
+    return <div className="section">
+        <h2 className="subtitle">Hello {name}.</h2>
+    </div>
 }
 
 // Counter app but using hooks and functional components
@@ -80,9 +80,10 @@ function CounterHooked() {
         setCounter(counter + 1);
     }
 
-    return <div>
-        <p>{counter}</p>
-        <button onClick={increment}>Increment</button>
+    return <div className="section">
+        <h2 className="subtitle">Counter using Hooks in Functional Components</h2>
+        <p className="p">{counter}</p>
+        <button className="button--increment" onClick={increment}>Increment</button>
     </div>
 }
 
@@ -93,26 +94,62 @@ class CounterLifecycleMethods extends React.Component {
     state = {
         counter: 0
     }
+    // Set initial value of the counter when Component is rendered
+    componentDidMount() {
+        this.setState({counter: 30});
+    }
+    componentDidUpdate() {
+        console.log("Number of clicks so far: " + this.state.counter);
+    }
     increment = () => {
         this.setState({counter: this.state.counter + 1});
     }
     render() {
-        return <div>
-            <h3>Counter with Lifecycle Methods</h3>
-            <p className="p--counter">{this.state.counter}</p>
+        return <div className="section">
+            <h2 className="subtitle">Counter with Lifecycle Methods</h2>
+            <p className="p">{this.state.counter}</p>
+            <p className="p">Checkout the console! 😁</p>
             <button className="button--increment" onClick={this.increment}>Increment</button>
         </div>
     }
 }
 
+// ------------------------------------------------------------------------
+// ------ useEffect: LIFECYCLE METHODS (BUT IN FUNC. COMPS)----------------
+// ------------------------------------------------------------------------
+function CounterUseEffect() {
+    const [counter, setCounter] = useState(0);
+    useEffect( () => {
+        // document.title = "Number of clicks so far: " + counter;
+        if (counter != 0) { // Little hack, only render Component if counter changes
+            alert("Number of clicks so far: " + counter);
+        }
+
+        return () => {
+            // To mimic componentWillUnmount, useEffect may return a function that cleans up after it:
+            // cleanup
+        }
+    }, [counter]); // Second arg indicates to component only will fires useEffect hook when the comp is rendered and also when $counter value changes.
+    const increment = () => {
+        setCounter(counter + 1);
+    }
+    return <div className="section">
+        <h2 className="subtitle">Counter using useEffect: Lifecycle methods in Functional Components</h2>
+        <p className="p">{counter}</p>
+        <p className="p">Look at the page title 😎</p>
+        <button className="button--increment" onClick={increment}>Increment</button>
+    </div>
+}
+
 
 root.render(
-    <>
-    <App></App>
-    <Hello></Hello>
-    <Counter></Counter>
-    <HelloFunctional></HelloFunctional>
-    <CounterHooked></CounterHooked>
-    <CounterLifecycleMethods></CounterLifecycleMethods>
-    </>
+    <div className="container">
+        <App></App>
+        <Hello></Hello>
+        <Counter></Counter>
+        <HelloFunctional></HelloFunctional>
+        <CounterHooked></CounterHooked>
+        <CounterLifecycleMethods></CounterLifecycleMethods>
+        <CounterUseEffect></CounterUseEffect>
+    </div>
 );
